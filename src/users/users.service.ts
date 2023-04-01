577bd368589edc96b94e570b65c6a19b9ef9e192
shunjid <shunjid.rahman@gmail.com>
2023-04-01 13:22:59 +0600

    Add controller and service for find, remove and update user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
@@ -10,5 +10,48 @@ export class UsersService {
   create(email: string, password: string) {
     const user = this.repo.create({ email, password });
     return this.repo.save(user);
+  }
+
+  async findOne(id: number) {
+    const user = await this.repo.findOne({
+      where: { id: id },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return user;
+  }
+
+  find(email: string) {
+    return this.repo.find({
+      where: { email: email },
+    });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.repo.findOne({
+      where: { id: id },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.repo.findOne({
+      where: { id: id },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return this.repo.remove(user);
   }
 }
